68a801275fceb668db7fd3cf1152cec10d236c3d
Dinolek <Dinolek@o2.pl>
2017-03-17 17:23:03 +0100

    Revert "Update"
    This reverts commit 6dcb855457d65b8b63b338d5cf4a46b4119024c0.

--- drivers/misc/mediatek/mach/mt6795/hermes/power/cust_pmic.h
+++ drivers/misc/mediatek/mach/mt6795/hermes/power/cust_pmic.h
@@ -3,12 +3,26 @@
 
 #define LOW_POWER_LIMIT_LEVEL_1 15
 
+//Define for disable low battery protect feature, default no define for enable low battery protect.
+//#define DISABLE_LOW_BATTERY_PROTECT
+
+//Define for disable battery OC protect feature, default no define for enable low battery protect.
+//#define DISABLE_BATTERY_OC_PROTECT
+
+//Define for disable battery 15% protect feature, default no define for enable low battery protect.
+//#define DISABLE_BATTERY_PERCENT_PROTECT
+
+
+/* ADC Channel Number */
 typedef enum {
+	//MT6331
 	AUX_TSENSE_31_AP = 	0x004,
 	AUX_VACCDET_AP,
 	AUX_VISMPS_1_AP,
 	AUX_ADCVIN0_AP,
 	AUX_HP_AP = 		0x009,
+        
+	//MT6332
 	AUX_BATSNS_AP = 	0x010,
 	AUX_ISENSE_AP,
 	AUX_VBIF_AP,
@@ -21,6 +35,7 @@ typedef enum {
 	AUX_SPK_ISENSE_AP,
 	AUX_SPK_THR_V_AP,
 	AUX_SPK_THR_I_AP,
+
 	AUX_VADAPTOR_AP =	0x027,        
 	AUX_TSENSE_31_MD = 	0x104,
 	AUX_ADCVIN0_MD = 	0x107,
@@ -34,4 +49,4 @@ typedef enum {
 	ADC_CHIP_MAX
 } upmu_adc_chip_list_enum;
 
-#endif
+#endif /* _CUST_PMIC_H_ */ 
